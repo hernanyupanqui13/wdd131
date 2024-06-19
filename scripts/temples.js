dcb87f558b7a2ff4d8876531891602f1191a69ec
hernanyupanqui13 <yup19001@byui.edu>
2024-06-19 13:04:03 -0500

    Improving styles and adding classes

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,9 +3,6 @@ const menuButton = document.querySelector('#menu');
 
 
 menuButton.addEventListener('click', () => {
-  console.log('clicked');
-  console.log(nav);
-  console.log(menuButton);
 	nav.classList.toggle('show');
 	menuButton.classList.toggle('open');
 });
